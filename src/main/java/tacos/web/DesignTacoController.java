24197ea2fdba6c58af8c619e74861397e1c91183
asharma42 <asharma42@xavient.com>
2019-01-04 17:00:29 +0530

    Added OrderForm to receive custom taco order

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -48,7 +48,10 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processDesign(Taco design) {
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if(errors.hasErrors()) {
+			return "design";
+		}
 		log.info("Processing design: " + design);
 		
 		return "redirect:/orders/current";
